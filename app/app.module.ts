595372c40d0d63c5276719d239715e814587e433
David Cottrell <cottrell@users.noreply.github.com>
2016-12-04 19:36:45 +0000

    update

--- app/app.module.ts
+++ app/app.module.ts
@@ -10,7 +10,10 @@ import { ChartsModule } from "ng2-charts"
 import { AppComponent } from './app.component';
 import { AppComponent2 } from './app.component2';
 
-@NgModule({
+  @NgModule({
+  // A component must belong to an NgModule in order for it to be usable by
+  // another component or application. To specify that a component is a member
+  // of an NgModule, you should list it in the declarations field of that NgModule.
   declarations: [AppComponent, AppComponent2],
   imports: [
     BrowserModule,
